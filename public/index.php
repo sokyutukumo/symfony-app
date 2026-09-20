867787ae8079b857cea4e7e97b49a07a2fd5f5cd
sokyutukumo <sokyutukumo@gmail.com>
2026-09-20 15:05:42 +0900

    Update page heading to v2

--- public/index.php
+++ public/index.php
@@ -1,4 +1,1 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-echo "<h1>Welcome to Symfony App deployed via ArgoCD & GHCR!</h1>";
-echo "<p>Deployed at: " . date('Y-m-d H:i:s') . "</p>";
+<?php echo '<h1>Version 2.0 Deployed via GitOps!</h1>';
